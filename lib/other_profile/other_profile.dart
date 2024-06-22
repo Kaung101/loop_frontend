f7ce9cc5ad57b7accc484b1f0c705ed0081810b1
SoeThandarLwin <soethandar.lwin@mail.kmutt.ac.th>
2024-06-23 05:38:25 +0700

    Link profile with direct messaging

--- lib/other_profile/other_profile.dart
+++ lib/other_profile/other_profile.dart
@@ -5,11 +5,13 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:loop/auth/auth_repo.dart';
 import 'package:loop/components/bottomNavigation.dart';
 import 'package:loop/components/colors.dart';
+import 'package:tuple/tuple.dart';
+
+import '../chat/direct_message_view.dart';
 
 class OtherProfile extends StatefulWidget {
-
   final String userId;
-  
+
   OtherProfile({super.key, required this.userId});
 
   @override
@@ -17,7 +19,6 @@ class OtherProfile extends StatefulWidget {
 }
 
 class _OtherProfileState extends State<OtherProfile> {
-   
   final AuthRepository _authRepository = AuthRepository();
 
   String? _username;
@@ -31,11 +32,11 @@ class _OtherProfileState extends State<OtherProfile> {
     _fetchUserData();
   }
 
-
   Future<void> _fetchUserData() async {
     try {
-      final userData = await _authRepository.fetchOtherProfileData(userId: widget.userId);
-            //final userData = await _authRepository.fetchUserData();
+      final userData =
+          await _authRepository.fetchOtherProfileData(userId: widget.userId);
+      //final userData = await _authRepository.fetchUserData();
 
       setState(() {
         _username = userData?['username'];
@@ -47,7 +48,6 @@ class _OtherProfileState extends State<OtherProfile> {
       print('Error fetching user data: $e');
     }
   }
- 
 
   @override
   Widget build(BuildContext context) {
@@ -66,13 +66,21 @@ class _OtherProfileState extends State<OtherProfile> {
                 onPressed: () => Navigator.pop(context),
               )
             : null,
-            //chat here!
+        //chat here!
         actions: [
           Padding(
             padding: const EdgeInsets.only(right: 10.0),
             child: IconButton(
-              icon: const Icon(CupertinoIcons.chat_bubble_fill, color: AppColors.primaryColor, size: 30),
-              onPressed: () {},
+              icon: const Icon(CupertinoIcons.chat_bubble_fill,
+                  color: AppColors.primaryColor, size: 30),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => DirectMessageView(
+                            userId:
+                                Tuple2<String, String>(userId, _username!))));
+              },
             ),
           ),
         ],
@@ -84,13 +92,11 @@ class _OtherProfileState extends State<OtherProfile> {
             _followerSection(),
             const SizedBox(height: 20),
             _nameSection(),
-           // const SizedBox(height: 20),
+            // const SizedBox(height: 20),
             ShowOtherPost(userId: userId, image: _profileImageUrl ?? ''),
           ],
         ),
       ),
-      
-      
     );
   }
 
@@ -105,38 +111,37 @@ class _OtherProfileState extends State<OtherProfile> {
               padding: const EdgeInsets.only(left: 30.0),
               child: CircleAvatar(
                 radius: 75,
-               backgroundImage: _profileImageUrl != null && _profileImageUrl!.isNotEmpty
+                backgroundImage: _profileImageUrl != null &&
+                        _profileImageUrl!.isNotEmpty
                     ? NetworkImage(
-                      'http://10.0.2.2:3000/media?media_id=$_profileImageUrl'
-                    )
+                        'http://10.0.2.2:3000/media?media_id=$_profileImageUrl')
                     : const AssetImage('image/logo.png') as ImageProvider,
               ),
             ),
           ],
         ),
         const SizedBox(width: 40),
-        
       ],
     );
-    
   }
+
   //show firstname and last name
-  Widget _nameSection(){
-    return  Row(
+  Widget _nameSection() {
+    return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(
           _firstName ?? 'First Name',
-          style: const  TextStyle(
+          style: const TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
             color: AppColors.textColor,
           ),
         ),
-         const SizedBox(width: 10),
-          Text(
-            _lastName ?? 'Last Name',
-          style: const  TextStyle(
+        const SizedBox(width: 10),
+        Text(
+          _lastName ?? 'Last Name',
+          style: const TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
             color: AppColors.textColor,
@@ -145,6 +150,7 @@ class _OtherProfileState extends State<OtherProfile> {
       ],
     );
   }
+
   Widget _buttonSection() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
@@ -201,25 +207,36 @@ class _OtherProfileState extends State<OtherProfile> {
 
 class ShowOtherPost extends StatelessWidget {
   final AuthRepository _authRepository = AuthRepository();
-  final String userId; 
+  final String userId;
   final String image;
+
   ShowOtherPost({super.key, required this.userId, required this.image});
 
   Future<List<dynamic>> getAllPost() async {
     final postList = await _authRepository.otherProfilePost(userId: userId);
-    return postList.map((post) => PostWidget(
-      username: post['user_name'] ?? '',
-      userId: post['user'] ?? '',
-      userImage: post['profileImage'] ?? '' , // Replace with actual data if available
-      postImageOne: 'http://10.0.2.2:3000/media?media_id=${post['original_photo']}', // Replace with actual data if available
-      postImageTwo: 'http://10.0.2.2:3000/media?media_id=${post['reference_photo']}', // Replace with actual data if available
-      status: post['artist_post'] == false ?  "Looking for artisit" : "Upcycled by me"  , // Replace with actual data if available
-      productName: post['name']?? '',
-      productPrice: post['price']?? '',
-      description: post['description']?? '',
-    )).toList();
+    return postList
+        .map((post) => PostWidget(
+              username: post['user_name'] ?? '',
+              userId: post['user'] ?? '',
+              userImage: post['profileImage'] ?? '',
+              // Replace with actual data if available
+              postImageOne:
+                  'http://10.0.2.2:3000/media?media_id=${post['original_photo']}',
+              // Replace with actual data if available
+              postImageTwo:
+                  'http://10.0.2.2:3000/media?media_id=${post['reference_photo']}',
+              // Replace with actual data if available
+              status: post['artist_post'] == false
+                  ? "Looking for artisit"
+                  : "Upcycled by me",
+              // Replace with actual data if available
+              productName: post['name'] ?? '',
+              productPrice: post['price'] ?? '',
+              description: post['description'] ?? '',
+            ))
+        .toList();
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<dynamic>>(
@@ -279,7 +296,9 @@ class PostWidget extends StatelessWidget {
         color: AppColors.backgroundColor,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15.0),
-          side: const BorderSide(color: AppColors.textColor, width: 1.0), // Set border color and width
+          side: const BorderSide(
+              color: AppColors.textColor,
+              width: 1.0), // Set border color and width
         ),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
@@ -288,26 +307,25 @@ class PostWidget extends StatelessWidget {
             children: [
               InkWell(
                 onTap: () {
-              
                   Navigator.push(
                     context,
                     MaterialPageRoute(
                       builder: (context) => OtherProfile(userId: userId),
                     ),
-
                   );
                 },
                 child: Row(
                   children: [
-                     ClipOval(
+                    ClipOval(
                       child: userImage != 'null' && userImage.isNotEmpty
-                        ? Image.network(
-                           'http://10.0.2.2:3000/media?media_id=$userImage',
-                            width: 60,
-                            height: 60,
-                            fit: BoxFit.cover,
-                          )
-                        :  Image.asset('image/logo.png', width: 60, height: 60),
+                          ? Image.network(
+                              'http://10.0.2.2:3000/media?media_id=$userImage',
+                              width: 60,
+                              height: 60,
+                              fit: BoxFit.cover,
+                            )
+                          : Image.asset('image/logo.png',
+                              width: 60, height: 60),
                     ),
                     const SizedBox(width: 5),
                     TextButton(
@@ -317,7 +335,8 @@ class PostWidget extends StatelessWidget {
                             if (states.contains(MaterialState.hovered)) {
                               return Colors.transparent;
                             }
-                            if (states.contains(MaterialState.focused) || states.contains(MaterialState.pressed)) {
+                            if (states.contains(MaterialState.focused) ||
+                                states.contains(MaterialState.pressed)) {
                               return Colors.transparent;
                             }
                             return null; // Defer to the widget's default.
@@ -327,7 +346,9 @@ class PostWidget extends StatelessWidget {
                       onPressed: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => OtherProfile(userId: userId)),
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  OtherProfile(userId: userId)),
                         );
                       },
                       child: Text(
@@ -344,81 +365,86 @@ class PostWidget extends StatelessWidget {
               ),
               SizedBox(height: 16),
               Row(
-  children: [
-    Expanded(
-      child: Padding(
-        padding: const EdgeInsets.only(bottom: 10.0), // Only bottom padding
-        child: Container(
-          decoration: BoxDecoration(
-            color: AppColors.tertiaryColor, // Set the background color
-            borderRadius: BorderRadius.circular(10.0), // Rounded corners
-          ),
-          padding: const EdgeInsets.only(bottom: 8.0),
-          child: Column(
-            children: [
-              ClipRRect(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(10.0),
-                  topRight: Radius.circular(10.0),
-                ), 
-                child: Image.network(
-                  postImageOne,
-                  height: 120,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      // Only bottom padding
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: AppColors.tertiaryColor,
+                          // Set the background color
+                          borderRadius:
+                              BorderRadius.circular(10.0), // Rounded corners
+                        ),
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(10.0),
+                                topRight: Radius.circular(10.0),
+                              ),
+                              child: Image.network(
+                                postImageOne,
+                                height: 120,
+                                width: double.infinity,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            const SizedBox(height: 8.0),
+                            const Text(
+                              'Before',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      // Only bottom padding
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: AppColors.tertiaryColor,
+                          // Set the background color
+                          borderRadius:
+                              BorderRadius.circular(10.0), // Rounded corners
+                        ),
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(10.0),
+                                topRight: Radius.circular(10.0),
+                              ),
+                              child: Image.network(
+                                postImageTwo,
+                                height: 120,
+                                width: double.infinity,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            const SizedBox(height: 8.0),
+                            const Text(
+                              'After',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 8.0),
-              const Text(
-                'Before',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    ),
-    const SizedBox(width: 16),
-    Expanded(
-      child: Padding(
-        padding: const EdgeInsets.only(bottom: 10.0), // Only bottom padding
-        child: Container(
-          decoration: BoxDecoration(
-            color: AppColors.tertiaryColor, // Set the background color
-            borderRadius: BorderRadius.circular(10.0), // Rounded corners
-          ),
-          padding: const EdgeInsets.only(bottom: 8.0),
-          child: Column(
-            children: [
-              ClipRRect(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(10.0),
-                  topRight: Radius.circular(10.0),
-                ), 
-                child: Image.network(
-                  postImageTwo,
-                  height: 120,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              const SizedBox(height: 8.0),
-              const Text(
-                'After',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    ),
-  ],
-)
-,
               SizedBox(height: 16),
               Row(
                 children: [
